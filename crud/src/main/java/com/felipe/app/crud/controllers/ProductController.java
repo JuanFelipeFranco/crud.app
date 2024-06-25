afbdefa6fefd8f00726f6ff4b8de063adf50752e
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-06-25 14:13:10 -0500

    Create controller

---
+++ crud/src/main/java/com/felipe/app/crud/controllers/ProductController.java
@@ -0,0 +1,55 @@
+package com.felipe.app.crud.controllers;
+
+import com.felipe.app.crud.entities.Product;
+import com.felipe.app.crud.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public List<Product> list() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> view(@PathVariable Long id) {
+        Optional<Product> productOptional = service.findById(id);
+        if (productOptional.isPresent()) {
+            return ResponseEntity.ok(productOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody Product product) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(product));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product) {
+        Optional<Product> productOptional = service.update(id, product);
+        if (productOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(productOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        Optional<Product> productOptional = service.delete(id);
+        if (productOptional.isPresent()) {
+            return ResponseEntity.ok(productOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
